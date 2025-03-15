8f81b9e5892c4185befaed017cd913949f58623d
Maria Paula Parra <mariapparra@gmail.com>
2025-03-14 20:37:06 -0500

    changes

--- Leaflet-Part-1/Starter_Code/static/js/logic.js
+++ Leaflet-Part-1/Starter_Code/static/js/logic.js
@@ -1,20 +1,28 @@
 const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson';
 
-const depthColors = ['#8f0', '#ee0', '#eb0', '#e80', '#b60', '#a00'];
-const depthThresholds = [0, 10, 30, 50, 70, 90];
+// Define color scale for magnitude
+const magnitudeColors = ['#8f0', '#ee0', '#eb0', '#e80', '#b60', '#a00'];
+const magnitudeThresholds = [0, 1, 2.5, 4, 5, 6];
 
-const getFillColor = depth => {
-    for (let i = depthThresholds.length - 1; i >= 0; i--) {
-        if (depth > depthThresholds[i]) {
-            return depthColors[i];
+const getFillColor = magnitude => {
+    for (let i = magnitudeThresholds.length - 1; i >= 0; i--) {
+        if (magnitude >= magnitudeThresholds[i]) {
+            return magnitudeColors[i];
         }
     }
+    return '#ccc'; // Default color if no match
+};
+
+const getRadius = magnitude => {
+    if (magnitude === 0) {
+        return 1; // Minimum visible radius
+    }
+    return magnitude * 3; // Adjust scaling factor if needed
 };
 
 const createMarkers = (earthquakeData, myMap) => {
     L.geoJSON(earthquakeData.features, {
         pointToLayer: (feature, latlng) => {
-            const depth = feature.geometry.coordinates[2];
             const magnitude = feature.properties.mag;
 
             const markerOptions = {
@@ -22,8 +30,8 @@ const createMarkers = (earthquakeData, myMap) => {
                 fillOpacity: 0.65,
                 color: 'black',
                 weight: 0.3,
-                fillColor: getFillColor(depth),
-                radius: magnitude * 3
+                fillColor: getFillColor(magnitude),
+                radius: getRadius(magnitude)
             };
 
             return L.circleMarker(latlng, markerOptions);
@@ -44,10 +52,10 @@ const createLegend = myMap => {
     legend.onAdd = () => {
         const div = L.DomUtil.create('div', 'legend');
         div.style.cssText = 'background-color: white; margin-left: 0px; padding: 0px 10px 0px 0px; border-radius: 5px;';
-        
+
         let legendHTMLString = "<ul style=\"list-style-type: none; padding-left: 10px;\">";
-        depthColors.forEach((color, index) => {
-            const range = index === 0 ? '< 10' : index === depthColors.length - 1 ? '90+' : `${depthThresholds[index]}-${depthThresholds[index+1]}`;
+        magnitudeColors.forEach((color, index) => {
+            const range = index === 0 ? '< 1' : index === magnitudeColors.length - 1 ? '6+' : `${magnitudeThresholds[index]}-${magnitudeThresholds[index+1]}`;
             legendHTMLString += `<li><span style=\"background-color: ${color};\">&emsp;</span> ${range}</li>`;
         });
         legendHTMLString += "</ul>";
